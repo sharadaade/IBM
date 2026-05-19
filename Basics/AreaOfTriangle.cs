62f41eecc53eca313bac113187de8a7a6f56bb66
sharadaade <aadesharad@email.com>
2026-05-19 17:35:05 +0530

    Add project files.

---
+++ Basics/AreaOfTriangle.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IBM.Basics
+{
+    internal class AreaOfTriangle
+    {
+        static void Main(string[] args)
+        {
+            //Console.Write("Enter the base : ");
+            //double b = Convert.ToDouble(Console.ReadLine());
+
+            //Console.Write("Enter the height : ");
+            //double h = Convert.ToDouble(Console.ReadLine());
+
+            //double result = 0.5 * b * h;
+
+            //Console.WriteLine($"Area of triangle is : {result}");
+
+            Console.Write("Enter the base : ");
+            decimal b = Convert.ToDecimal(Console.ReadLine());
+
+            Console.Write("Enter the height : ");
+            decimal h = Convert.ToDecimal(Console.ReadLine());
+
+            decimal result = 0.5m * b * h;
+
+            Console.WriteLine($"Area of triangle is : {result}");
+        }
+    }
+}
