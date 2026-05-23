9473b66c632fa5daa1c1f463c39c7d4dd3d06e9b
sharadaade <aadesharad@email.com>
2026-05-23 22:33:19 +0530

    legth and temprature convert

---
+++ Basics/LengthConvertor.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IBM.Basics
+{
+    internal class LengthConvertor
+    {
+        public void Converter(decimal input)
+        {
+            Console.WriteLine($"Length in : { input / 100:F4} m");
+            Console.WriteLine($"Length in : {input/ 100000:F4} Km");
+            
+        }
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the length in CM : ");
+            decimal input = Convert.ToDecimal(Console.ReadLine());
+
+            LengthConvertor obj = new LengthConvertor();
+            obj.Converter(input);
+        }
+    }
+}
---
+++ Basics/TempratureConvertor.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IBM.Basics
+{
+    internal class TempratureConvertor
+    {
+        public int Convert(int cel)
+        {
+            return ((cel * 9 / 5) + 32);
+        }
+        static void Main(string[] args)
+        {
+            int num;
+            string input;
+
+            Console.Write("Please enter temprature in celcius : ");
+            input = Console.ReadLine();
+
+            if(int.TryParse(input, out num))
+            {
+                TempratureConvertor obj = new TempratureConvertor();
+                int Fahrenheit = obj.Convert(num);
+                Console.Write("Temprature in Fahrenheit : " + Fahrenheit);
+            }
+            else
+            {
+                Console.WriteLine("Please enter the valid input.");
+            }
+        }
+    }
+}
