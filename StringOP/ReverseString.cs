d565d029dd506e608b74d49b3f294e51baea69a8
sharadaade <aadesharad@email.com>
2026-05-27 20:48:18 +0530

    string reverse

---
+++ StringOP/ReverseString.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IBM.StringOP
+{
+    internal class ReverseString
+    {
+        static void Main(string[] args)
+        {
+            string str = "Hello World";
+            char[] ar = str.ToCharArray();
+
+            int start = 0;
+            int end = str.Length - 1;
+
+            while(start < end)
+            {
+                char temp = ar[start];
+                ar[start] = ar[end];
+                ar[end] = temp;
+
+                start++; end--;
+            }
+
+            string reverse = new String(ar);
+            Console.WriteLine(reverse);
+        }
+    }
+}
