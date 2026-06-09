325257458ef682de4ecf5ad7157e126778ed5398
sharadaade <aadesharad@email.com>
2026-06-09 22:16:27 +0530

    even-odd with int.TryParse

---
+++ Basics/EvenOdd.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IBM.Basics
+{
+    internal class EvenOdd
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the number : ");
+            string? strNumber = Console.ReadLine();
+
+            if(int.TryParse(strNumber, out int newNumber))
+            {
+                if(newNumber % 2 == 0)
+                {
+                    Console.WriteLine("The number is Even");
+                }
+                else
+                {
+                    Console.WriteLine("The number is Odd");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Invalid input please enter the valid number");
+            }
+
+            //Console.WriteLine(newNumber);
+        }
+    }
+}
---
+++ Basics/SwapTwoNumber.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IBM.Basics
+{
+    internal class SwapTwoNumber
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the a value : ");
+            int a = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter the b value : ");
+            int b = Convert.ToInt32(Console.ReadLine());
+
+            SwapTwoNumber obj = new SwapTwoNumber();
+
+            obj.SwapWithTwoVariable(a, b);
+            obj.SwapWithouTwoVariable(a, b);
+
+        }
+
+        public void SwapWithTwoVariable(int a, int b)
+        {
+            int temp = a;
+            a = b;
+            b = temp;
+
+            Console.WriteLine($"Value of a : {a} \nValue of b : {b}");
+        }
+
+        public void SwapWithouTwoVariable(int a, int b)
+        {
+            // Tuple
+            (a, b) = (b, a);
+            Console.WriteLine($"a = {a}");
+            Console.WriteLine($"b = {b}");
+        }
+    }
+}
