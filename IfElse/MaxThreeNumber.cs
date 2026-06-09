db38f90b4f09db8856593f91aa5d7fb70e6257a4
sharadaade <aadesharad@email.com>
2026-06-10 02:01:05 +0530

    max-of-two-and-three-number

---
+++ IfElse/MaxThreeNumber.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IBM.IfElse
+{
+    internal class MaxThreeNumber
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the first number : ");
+            int first = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter the second number : ");
+            int second = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter the second number : ");
+            int third = Convert.ToInt32(Console.ReadLine());
+
+            if(first > second && first > third)
+            {
+                Console.WriteLine($"Max number is : {first}");
+            }
+            else if(second > third)
+            {
+                Console.WriteLine($"Max number is : {second}");
+            }
+            else
+            {
+                Console.WriteLine($"Max number is : {third}");
+            }
+        }
+    }
+}
---
+++ IfElse/MaxTwoNumber.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IBM.IfElse
+{
+    internal class MaxTwoNumber
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the first number : ");
+            int first = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter the second number : ");
+            int second = Convert.ToInt32(Console.ReadLine());
+
+            if(first > second)
+            {
+                Console.WriteLine($"Max number is : {first}");
+            }
+            else
+            {
+                Console.WriteLine($"Max number is : {second}");
+            }
+        }
+    }
+}
