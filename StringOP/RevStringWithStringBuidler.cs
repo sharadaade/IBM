b80fd427b38d144f0cfa1a08164fd1ee54c560b8
sharadaade <aadesharad@email.com>
2026-06-18 23:19:11 +0530

    reverse string with StringBuilder instance - it is mutable

---
+++ StringOP/RevStringWithStringBuidler.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IBM.StringOP
+{
+    internal class RevStringWithStringBuidler
+    {
+        static void Main(string[] args)
+        {
+            string str = "Hello"; // string is immutable
+
+            // StringBuilder instance => it is mutable
+            StringBuilder rev = new StringBuilder();
+
+            //      4                   -1 >= 0
+            for (int i = str.Length - 1; i >= 0; i--)
+            {
+                rev.Append(str[i]); // olleH
+            }
+            Console.WriteLine(rev);
+        }
+    }
+}
