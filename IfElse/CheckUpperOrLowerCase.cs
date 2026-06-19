3b15ec51c4b46621c964979b5542130554a9238a
sharadaade <aadesharad@email.com>
2026-06-19 22:06:29 +0530

    check upper or lower case

---
+++ IfElse/CheckUpperOrLowerCase.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IBM.IfElse
+{
+    internal class CheckUpperOrLowerCase
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter character to check Uppercase or Lowercase :  ");
+            char ch = Console.ReadKey().KeyChar;
+            Console.WriteLine();
+
+            if (ch >= 'a' && ch <= 'z')
+            {
+                Console.WriteLine("lowercase");
+            }
+            else if(ch >= 'A' && ch <= 'Z')
+            {
+                Console.WriteLine("UPPERCASE");
+            }
+            else
+            {
+                Console.WriteLine("Not an alphabet character");
+            }
+        }
+    }
+}
---
+++ IfElse/VowelConsonant.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IBM.IfElse
+{
+    internal class VowelConsonant
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the character : ");
+            char ch = Console.ReadKey().KeyChar;
+            
+            if(ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' || ch == 'A' || ch == 'E' || ch == 'I' || ch == 'O' || ch == 'U')
+            {
+                Console.WriteLine("Vowel");
+            }
+            else
+            {
+                Console.WriteLine("Consonant");
+            }
+        }
+    }
+}
