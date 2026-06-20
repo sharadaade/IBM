7862cf33188b4805128ec1c5d40abf92dde1dfe6
sharadaade <aadesharad@email.com>
2026-06-20 22:49:01 +0530

    day of week

---
+++ IfElse/DayOfWeek.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IBM.IfElse
+{
+    internal class DayOfWeek
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the day number : ");
+            string strDay = Console.ReadLine();
+
+            if(int.TryParse(strDay, out int day))
+            {
+                if(day == 1)
+                {
+                    Console.WriteLine("Monday");
+                }
+                else if(day == 2)
+                {
+                    Console.WriteLine("Tuesday");
+                }
+                else if (day == 3)
+                {
+                    Console.WriteLine("Wednesday");
+                }
+                else if (day == 4)
+                {
+                    Console.WriteLine("Thusday");
+                }
+                else if (day == 5)
+                {
+                    Console.WriteLine("Friday");
+                }
+                else if (day == 6)
+                {
+                    Console.WriteLine("Saturday");
+                }
+                else if (day == 7)
+                {
+                    Console.WriteLine("Sunday");
+                }
+                else
+                {
+                    Console.WriteLine("Invalid Day");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Invalid Input");
+            }
+        }
+    }
+}
