a1ccbeed792c535d8f538d0f43825bbeb5b99be2
sharadaade <aadesharad@email.com>
2026-06-22 18:02:10 +0530

    PrintYesterDayDate

---
+++ Basics/PrintYesterDayDate.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IBM.Basics
+{
+    internal class PrintYesterDayDate
+    {
+        static void Main(string[] args)
+        {
+            
+            DateTime Today = DateTime.Today;
+
+            DateTime YesterDay = DateTime.Today.AddDays(-1);
+
+            Console.WriteLine(Today);
+            Console.WriteLine(YesterDay);
+
+        }
+    }
+}
