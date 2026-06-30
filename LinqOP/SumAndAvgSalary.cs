cdb4825f97daa4f3e619fee7a6c3b53eb8d4a1af
sharadaade <aadesharad@email.com>
2026-06-30 20:20:35 +0530

    sum and average with linq

---
+++ LinqOP/SumAndAvgSalary.cs
@@ -0,0 +1,25 @@
+﻿using IBM.Data;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IBM.LinqOP
+{
+    internal class SumAndAvgSalary
+    {
+        static void Main(string[] args)
+        {
+            var employees = DataList.GetEmployees();
+
+            // Sum of all employees salary
+            var SumOfSalary = employees.Sum(x => x.Salary);
+            Console.WriteLine($"Sum of salary : {SumOfSalary}$");
+
+            Console.WriteLine();
+
+            var AverageSalary = employees.Average(x => x.Salary);
+            Console.WriteLine($"Average Salary : {AverageSalary}$");
+
+        }
+    }
+}
