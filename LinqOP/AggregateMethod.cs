b4b5d1649c4a2182c9c221b7a4d2e681f01fe3ba
Sharad Aade <s.aade@vipspms.com>
2026-09-11 13:00:51 +0530

    ok

---
+++ LinqOP/AggregateMethod.cs
@@ -0,0 +1,17 @@
+﻿namespace IBM.LinqOP
+{
+    internal class AggregateMethod
+    {
+        static void Main(string[] args)
+        {
+            string[] fruits = { "apple", "mango", "orange", "passion", "grapes" };
+
+            string longestFruit = fruits.Aggregate("banana",
+                                                    (longest, next) =>
+                                                     next.Length > longest.Length ? next : longest,
+                                                    fruit => fruit.ToUpper());
+
+            Console.WriteLine(longestFruit);
+        }
+    }
+}
