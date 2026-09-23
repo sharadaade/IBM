5278744b0d602da6ef47da740514ba77977a6726
Sharad Aade <s.aade@vipspms.com>
2026-09-23 22:35:50 +0530

    address check

---
+++ Concept/IPAddressCheck.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IBM.Concept
+{
+    internal class IPAddressCheck
+    {
+        static void Main(string[] args)
+        {
+            var httpclient = new HttpClient();
+            var ip = httpclient.GetStringAsync("https://api.ipify.org");
+            Console.WriteLine($"My public IP address is: {ip}");
+        }
+    }
+}
